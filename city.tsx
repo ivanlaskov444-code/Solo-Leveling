201548b5061752056df16a685d913dd3abc32175
ivanlaskov444-code <ivan.laskov444@gmail.com>
2026-01-23 20:34:46 +0300

    Add files via upload

---
+++ city.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="city" tilewidth="32" tileheight="32" tilecount="1040" columns="40">
+ <image source="kenney_rpg-base/Spritesheet/RPGpack_sheet.png" width="1280" height="832"/>
+</tileset>
